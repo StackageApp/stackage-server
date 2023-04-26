1f356753826811db10333fbd1629ddd94090c1cc
mevcaus <mevludincausevic@gmail.com>
2023-04-26 17:45:48 -0600

    adding cors policy and check for specific userid on get posts endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(router);
